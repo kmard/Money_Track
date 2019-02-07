b016e570c83412b0bbe5ea499a188ecd351e9d7f
admin <tsnok777@gmail.com>
2019-02-07 17:14:55 +0200

    added onStart, onResume, onPause, onStop, onDestroy for activity

--- Money/src/main/java/com/example/money/MainActivity.java
+++ Money/src/main/java/com/example/money/MainActivity.java
@@ -1,7 +1,8 @@
 package com.example.money;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStop() {
+        Toast.makeText(this,"onStop()",Toast.LENGTH_SHORT).show();
+        super.onStop();
+    }
+
+    @Override
+    protected void onRestart() {
+        Toast.makeText(this,"onRestart()",Toast.LENGTH_SHORT).show();
+        super.onRestart();
+    }
+
+    @Override
+    protected void onResume() {
+        Toast.makeText(this,"onResume()",Toast.LENGTH_SHORT).show();
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        Toast.makeText(this,"onPause()",Toast.LENGTH_SHORT).show();
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Toast.makeText(this,"onDestroy()",Toast.LENGTH_SHORT).show();
+        super.onDestroy();
     }
 }
