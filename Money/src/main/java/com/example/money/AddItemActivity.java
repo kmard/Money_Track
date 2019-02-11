7c40bf375b0d896d82c5fe76f299f33749e6e3ba
admin <tsnok777@gmail.com>
2019-02-11 13:09:53 +0200

    hWork2

--- Money/src/main/java/com/example/money/AddItemActivity.java
+++ Money/src/main/java/com/example/money/AddItemActivity.java
@@ -2,12 +2,59 @@ package com.example.money;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class AddItemActivity extends AppCompatActivity {
+public class AddItemActivity extends AppCompatActivity   {
 
+    EditText name;
+    EditText price;
+    Button addBtn;
+
+    private static final String TAG = "AddItemActivity";
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_additem);
+
+        //setTitle(R.string.add_item_title);
+
+        name = (EditText) findViewById(R.id.name);
+        price = (EditText) findViewById(R.id.price);
+        addBtn = (Button) findViewById(R.id.add_btn);
+        
+
+        View.OnClickListener oclBtnOk = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String itemName = name.getText().toString();
+                String itemPrice = price.getText().toString();
+            }
+        };
+        
+        addBtn.setOnClickListener(oclBtnOk);
+        
+        name.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                Log.i(TAG, "beforeTextChanged: ");
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                Log.i(TAG, "onTextChanged: ");
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                Log.i(TAG, "afterTextChanged: ");
+            }
+        });
+        
+       }
     }
-}
