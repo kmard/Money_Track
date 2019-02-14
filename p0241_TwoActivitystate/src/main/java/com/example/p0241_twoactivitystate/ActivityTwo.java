1e3cf073817b74dbc76233f9641aee2986615764
admin <tsnok777@gmail.com>
2019-02-14 14:10:29 +0200

    twoActivity

--- p0241_TwoActivitystate/src/main/java/com/example/p0241_twoactivitystate/ActivityTwo.java
+++ p0241_TwoActivitystate/src/main/java/com/example/p0241_twoactivitystate/ActivityTwo.java
@@ -1,4 +1,4 @@
-package com.example.twoactivity;
+package com.example.p0241_twoactivitystate;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -7,60 +7,60 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
-
 public class ActivityTwo extends AppCompatActivity implements View.OnClickListener {
-     Button btn;
-    private static final String TAG = "TwoActivity";
+
+    private static final String TAG = "State ";
+    Button btActMain;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_two);
-        btn = (Button) findViewById(R.id.btn);
-        btn.setOnClickListener(this);
-        Log.i(TAG, "onCreate: Second activity");
+        btActMain = (Button) findViewById(R.id.btActMain);
+        btActMain.setOnClickListener(this);
+        Log.i(TAG, "onCreate: Activity two");
     }
 
     @Override
-    public void onClick(View v) {
-        switch (v.getId()){
-            case R.id.btn:
-                Intent intent = new Intent(this,MainActivity.class);
-                    startActivity(intent);
-                Log.i(TAG, "onClick: return MainActivity");   
-                break;
-             default:
-
-               break;
-        }
+    protected void onRestart() {
+        Log.i(TAG, "onRestart: Activity two");
+        super.onRestart();
     }
 
     @Override
     protected void onStart() {
-        Log.i(TAG, "onStart: TwoActivity");
+        Log.i(TAG, "onStart: Activity two");
         super.onStart();
     }
 
     @Override
     protected void onResume() {
-        Log.i(TAG, "onResume: TwoActivity");
+        Log.i(TAG, "onResume: Activity two");
         super.onResume();
     }
 
     @Override
     protected void onPause() {
-        Log.i(TAG, "onPause: TwoActivity");
+        Log.i(TAG, "onPause: Activity two");
         super.onPause();
     }
 
     @Override
     protected void onStop() {
-        Log.i(TAG, "onStop: TwoActivity");
+        Log.i(TAG, "onStop: Activity two");
         super.onStop();
     }
 
     @Override
     protected void onDestroy() {
-        Log.i(TAG, "onDestroy: TwoActivity");
+        Log.i(TAG, "onDestroy: Activity two");
         super.onDestroy();
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
     }
 }
