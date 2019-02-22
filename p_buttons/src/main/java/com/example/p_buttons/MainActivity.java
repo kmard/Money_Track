aecb449dcb76f8ef70e52abc8c93b3790b5e3756
admin <tsnok777@gmail.com>
2019-02-22 12:37:45 +0200

    p0342_simplesqlite_update_delete

--- p_buttons/src/main/java/com/example/p_buttons/MainActivity.java
+++ p_buttons/src/main/java/com/example/p_buttons/MainActivity.java
@@ -4,37 +4,37 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.Toast;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
-    Button btnBlue;
-    CheckBox checkBox;
+    Button btnMain;
+
+    private int mButton = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnBlue = (Button) findViewById(R.id.btnBlue);
-        btnBlue.setOnClickListener(this);
+        btnMain = (Button) findViewById(R.id.btnMain);
+        btnMain.setOnClickListener(this);
 
-        checkBox = (CheckBox) findViewById(R.id.checkBox);
-        checkBox.setOnClickListener(this);
+
 
     }
 
     @Override
     public void onClick(View v) {
-         switch(v.getId()){
-             case R.id.btnBlue:
-                 Toast.makeText(this,"btnBlue",Toast.LENGTH_LONG).show();
-                 break;
-             case R.id.checkBox:
-                 Toast.makeText(this,"checkBox",(int)2/2).show();
-                 break;
-         }
+            if (v.getId() == R.id.btnMain){
+                Button newButton = new Button(this);
+                newButton.setOnClickListener(this);
+                newButton.setText("Button # "+(mButton++));
+                ((LinearLayout) v.getParent()).addView(newButton);
+            }  else{
+                ((LinearLayout) v.getParent()).removeView(v);
+            }
+
 
     }
 
