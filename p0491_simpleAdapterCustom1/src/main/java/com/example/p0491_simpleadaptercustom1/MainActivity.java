0c3f0493073d900d04f373f3fd990b263d479d73
kmard <tsnok777@gmail.com>
2019-03-05 12:43:06 +0200

    p0501_simpleadaptercustom2

--- p0491_simpleAdapterCustom1/src/main/java/com/example/p0491_simpleadaptercustom1/MainActivity.java
+++ p0491_simpleAdapterCustom1/src/main/java/com/example/p0491_simpleadaptercustom1/MainActivity.java
@@ -1,17 +1,22 @@
 package com.example.p0491_simpleadaptercustom1;
 
 
+import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 
 public class MainActivity extends AppCompatActivity {
-
 
     //name attributes
     final String ATTRIBUTE_NAME_TEXT = "text";
@@ -57,11 +62,60 @@ public class MainActivity extends AppCompatActivity {
             int[] to = {R.id.tvText, R.id.tvValue, R.id.ivImg};
 
             //create adapter
-            //MySimpleAdapter sAdapter = new MySimpleAdapter(this, data, android.R.layout.activity_item,from,to);
+            MySimpleAdapter sAdapter = new MySimpleAdapter(this, data, R.layout.activity_items, from, to);
 
-
+            //add adapter to the lvSimple
+            lvSimple = (ListView) findViewById(R.id.lvSimple);
+            lvSimple.setAdapter(sAdapter);
         }
 
+    }
+
+    class MySimpleAdapter extends SimpleAdapter {
+        /**
+         * Constructor
+         *
+         * @param context  The context where the View associated with this SimpleAdapter is running
+         * @param data     A List of Maps. Each entry in the List corresponds to one row in the list. The
+         *                 Maps contain the data for each row, and should include all the entries specified in
+         *                 "from"
+         * @param resource Resource identifier of a view layout that defines the views for this list
+         *                 item. The layout file should include at least those named views defined in "to"
+         * @param from     A list of column names that will be added to the Map associated with each
+         *                 item.
+         * @param to       The views that should display column in the "from" parameter. These should all be
+         *                 TextViews. The first N views in this list are given the values of the first N columns
+         */
+        public MySimpleAdapter(Context context, List<? extends Map<String, ?>> data, int resource, String[] from, int[] to) {
+            super(context, data, resource, from, to);
+        }
+
+        @Override
+        public void setViewText(TextView v, String text) {
+            //method superClassa which insert text
+            super.setViewText(v, text);
+            //if needed us TextView, then painting
+            if (v.getId() == R.id.tvValue) {
+                int i = Integer.parseInt(text);
+                if (i < 0) {
+                    v.setTextColor(Color.RED);
+                } else if (i > 0) {
+                    v.setTextColor(Color.GREEN);
+                }
+            }
+        }
+
+        @Override
+        public void setViewImage(ImageView v, int value) {
+            super.setViewImage(v, value);
+            //painting
+            if (value == negative) {
+                v.setBackgroundColor(Color.RED);
+            } else if (value == positive) {
+                v.setBackgroundColor(Color.GREEN);
+            }
+
+        }
     }
 }
 
