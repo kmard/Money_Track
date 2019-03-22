40c2067ae8c73cdc2e3b076c244a44ae17af1433
kmard <tsnok777@gmail.com>
2019-03-22 19:35:24 +0200

    p0691_Parcelable

---
+++ p0691_Parcelable/src/main/java/com/example/p0691_parcelable/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.p0691_parcelable;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view) {
+        MyObject myObj = new MyObject("text", 1);
+
+    }
+}
