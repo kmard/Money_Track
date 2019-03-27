6b57455662f7a72f5d2d5046a4256f00bab005c7
kmard <tsnok777@gmail.com>
2019-03-27 08:43:40 +0200

    p0741_PreferencesCode

---
+++ p0741_PreferencesCode/src/main/java/com/example/p0741_preferencescode/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.p0741_preferencescode;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuItem mi = menu.add(0, 1, 0, "Preferences");
+        mi.setIntent(new Intent(this, PrefActivity.class));
+        return super.onCreateOptionsMenu(menu);
+    }
+}
