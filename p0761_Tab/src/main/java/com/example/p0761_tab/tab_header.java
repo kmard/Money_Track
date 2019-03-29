92dc15e53afae67b6cde2781e5dd3ffcaf1da1f0
kmard <tsnok777@gmail.com>
2019-03-29 08:48:07 +0200

    p0761_Tab

---
+++ p0761_Tab/src/main/java/com/example/p0761_tab/tab_header.java
@@ -0,0 +1,13 @@
+package com.example.p0761_tab;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class tab_header extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tab_header);
+    }
+}
