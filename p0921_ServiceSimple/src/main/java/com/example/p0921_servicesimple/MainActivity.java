dd55e706e933c43c57edda81d20430e96c4d39df
kmard <tsnok777@gmail.com>
2019-04-16 08:54:50 +0300

    p0921_ServiceSimple

--- p0921_ServiceSimple/src/main/java/com/example/p0921_servicesimple/MainActivity.java
+++ p0921_ServiceSimple/src/main/java/com/example/p0921_servicesimple/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.p0921_servicesimple;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "myLogs";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClickStop(View v) {
+        Log.i(TAG, "stopService ");
+        stopService(new Intent(this, MyService.class));
+    }
+
+    public void onClickStart(View v) {
+        Log.i(TAG, "startService ");
+        startService(new Intent(this, MyService.class));
     }
 }
