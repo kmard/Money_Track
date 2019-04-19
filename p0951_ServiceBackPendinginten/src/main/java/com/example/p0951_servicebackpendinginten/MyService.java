1234363e6f48df0b4dd673ab1de0ae2c2c7712e0
kmard <tsnok777@gmail.com>
2019-04-19 09:08:34 +0300

    p0951_ServiceBackPendinginten

---
+++ p0951_ServiceBackPendinginten/src/main/java/com/example/p0951_servicebackpendinginten/MyService.java
@@ -0,0 +1,16 @@
+package com.example.p0951_servicebackpendinginten;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+
+public class MyService extends Service {
+
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
