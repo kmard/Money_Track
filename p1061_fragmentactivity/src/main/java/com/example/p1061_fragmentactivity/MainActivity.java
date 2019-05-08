526bbb68b8adbc56fbcc89c04dbaefa280419eb4
kmard <tsnok777@gmail.com>
2019-05-08 09:14:30 +0300

    p1061_fragmentactivity

--- p1061_fragmentactivity/src/main/java/com/example/p1061_fragmentactivity/MainActivity.java
+++ p1061_fragmentactivity/src/main/java/com/example/p1061_fragmentactivity/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.p1061_fragmentactivity;
 
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -9,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Fragment frag2 = new fragment2();
+        FragmentTransaction ft = getFragmentManager().beginTransaction();
+        //ft.add(R.id.fragment2, frag2);
+        //ft.add(R.id.fragment2, frag2);
+        ft.commit();
+
     }
 }
