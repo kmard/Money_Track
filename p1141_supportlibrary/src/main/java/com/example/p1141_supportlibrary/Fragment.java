6fe11b1b5b29c60316fa6beb3f7cc05ef0959cbe
kmard <tsnok777@gmail.com>
2019-05-17 09:21:56 +0300

    p1141_supportlibrary

--- p1141_supportlibrary/src/main/java/com/example/p1141_supportlibrary/Fragment.java
+++ p1141_supportlibrary/src/main/java/com/example/p1141_supportlibrary/Fragment.java
@@ -1,13 +1,18 @@
 package com.example.p1141_supportlibrary;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-public class Fragment extends AppCompatActivity {
-
+public class Fragment extends android.support.v4.app.Fragment {
+    @Nullable
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_fragment);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.activity_fragment, null);
     }
+
+
 }
--- p1141_supportlibrary/src/main/java/com/example/p1141_supportlibrary/MainActivity.java
+++ p1141_supportlibrary/src/main/java/com/example/p1141_supportlibrary/MainActivity.java
@@ -1,13 +1,23 @@
 package com.example.p1141_supportlibrary;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.Menu;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Fragment myFrag = new Fragment();
+        getSupportFragmentManager().beginTransaction().replace(R.id.cont, myFrag).commit();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.context, menu);
+        return true;
     }
 }
