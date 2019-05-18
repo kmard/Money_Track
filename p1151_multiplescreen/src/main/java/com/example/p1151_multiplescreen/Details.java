2317828148a2ca03f56a37fc5bde8d6ac7f298f5
kmard <tsnok777@gmail.com>
2019-05-18 12:26:35 +0300

    p1151_multiplescreen

--- p1151_multiplescreen/src/main/java/com/example/p1151_multiplescreen/Details.java
+++ p1151_multiplescreen/src/main/java/com/example/p1151_multiplescreen/Details.java
@@ -1,13 +1,36 @@
 package com.example.p1151_multiplescreen;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
-public class Details extends AppCompatActivity {
+public class Details extends Fragment {
 
+
+    public static Details newInstance(int pos) {
+        Details details = new Details();
+        Bundle args = new Bundle();
+        args.putInt("position", pos);
+        details.setArguments(args);
+        return details;
+    }
+
+    int getPosition() {
+        return getArguments().getInt("position", 0);
+    }
+
+    @Nullable
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_details);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.activity_details, container, false);
+        TextView tv = (TextView) v.findViewById(R.id.tvText);
+        tv.setText(getResources().getStringArray(R.array.content)[getPosition()]);
+        return v;
+        //return super.onCreateView(inflater, container, savedInstanceState);
     }
 }
--- p1151_multiplescreen/src/main/java/com/example/p1151_multiplescreen/MainActivity.java
+++ p1151_multiplescreen/src/main/java/com/example/p1151_multiplescreen/MainActivity.java
@@ -1,13 +1,46 @@
 package com.example.p1151_multiplescreen;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.os.PersistableBundle;
+import android.support.v4.app.FragmentActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity implements TitlesFragment.onItemClickListener {
+
+    int position = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (savedInstanceState != null)
+            position = savedInstanceState.getInt("position");
+        ShowDetails(position);
     }
+
+    private void ShowDetails(int pos) {
+        Details details = (Details) getSupportFragmentManager().findFragmentById(R.id.cont);
+        if (details == null || details.getPosition() != pos) {
+            details = Details.newInstance(pos);
+            getSupportFragmentManager().beginTransaction().replace(R.id.cont, details).commit();
+        }
+    }
+
+    @Override
+    public void itemClick(int position) {
+        this.position = position;
+        ShowDetails(position);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState) {
+        super.onSaveInstanceState(outState, outPersistentState);
+        outState.putInt("position", position);
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
+    }
+
+
 }
