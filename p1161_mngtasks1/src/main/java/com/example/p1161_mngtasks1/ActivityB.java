8eb0ed46073b80c3101666c56feafffc741efc9d
kmard <tsnok777@gmail.com>
2019-05-19 11:26:59 +0300

    p1162_mngtasks2

--- p1161_mngtasks1/src/main/java/com/example/p1161_mngtasks1/ActivityB.java
+++ p1161_mngtasks1/src/main/java/com/example/p1161_mngtasks1/ActivityB.java
@@ -1,13 +1,19 @@
 package com.example.p1161_mngtasks1;
 
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.view.View;
 
-public class ActivityB extends AppCompatActivity {
-
+public class ActivityB extends MainActivity {
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_b);
+    public void onClick(View v) {
+        startActivity(new Intent(this, ActivityC.class));
     }
+
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_b);
+//    }
+
+
 }
--- p1162_mngtasks2/src/main/java/com/example/p1162_mngtasks2/MainActivity.java
+++ p1162_mngtasks2/src/main/java/com/example/p1162_mngtasks2/MainActivity.java
@@ -1,13 +1,42 @@
 package com.example.p1162_mngtasks2;
 
+import android.app.ActivityManager;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "myLogs";
+    List<ActivityManager.RunningTaskInfo> list;
+    ActivityManager am;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle(getResources().getString(R.string.app_name) + " : " + getLocalClassName());
+        am = (ActivityManager) getSystemService(ACTIVITY_SERVICE);
     }
+
+    public void onClick(View view) {
+        startActivity(new Intent("mngtasks1_activity_c"));
+    }
+
+    public void onInfoClick(View view) {
+        list = am.getRunningTasks(10);
+        for (ActivityManager.RunningTaskInfo task : list) {
+            if (task.baseActivity.flattenToShortString().startsWith("com.example.p1162_mngtasks2")) {
+                Log.d(TAG, "------------------");
+                Log.d(TAG, "Count: " + task.numActivities);
+                Log.d(TAG, "Root: " + task.baseActivity.flattenToShortString());
+                Log.d(TAG, "Top: " + task.topActivity.flattenToShortString());
+            }
+        }
+    }
+
 }
