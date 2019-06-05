1f4225717e132987bb3d35a6b640c44f867c9b65
kmard <tsnok777@gmail.com>
2019-06-05 08:58:03 +0300

    p1281_audiofocus

--- p1281_audiofocus/src/main/java/com/example/p1281_audiofocus/MainActivity.java
+++ p1281_audiofocus/src/main/java/com/example/p1281_audiofocus/MainActivity.java
@@ -10,7 +10,7 @@ import android.view.View;
 
 import java.io.IOException;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements MediaPlayer.OnCompletionListener {
 
     private static final String TAG = "myLogs";
 
@@ -53,5 +53,65 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onClickSound(View view) {
+        int durationHint = AudioManager.AUDIOFOCUS_GAIN;
+        switch (view.getId()) {
+            case R.id.btnPlaySoundG:
+                durationHint = AudioManager.AUDIOFOCUS_GAIN;
+                break;
+            case R.id.btnPlaySoundGT:
+                durationHint = AudioManager.AUDIOFOCUS_GAIN_TRANSIENT;
+                break;
+            case R.id.btnPlaySoundGTD:
+                durationHint = AudioManager.AUDIOFOCUS_GAIN_TRANSIENT_MAY_DUCK;
+                break;
+        }
+
+        mpSound = MediaPlayer.create(this, R.raw.explosion);
+        mpSound.setOnCompletionListener(this);
+
+        afListenerSound = new AFListener(mpSound, "Sound");
+        int requestResult = audioManager.requestAudioFocus(afListenerSound,
+                AudioManager.STREAM_MUSIC, durationHint);
+        Log.i(TAG, "Sound request focus, result: " + requestResult);
+
+        mpSound.start();
+    }
+
+    @Override
+    public void onCompletion(MediaPlayer mp) {
+
+    }
+
+    private class AFListener implements AudioManager.OnAudioFocusChangeListener {
+
+        String label = "";
+        MediaPlayer mp;
+
+
+        public AFListener(MediaPlayer mp, String label) {
+            this.label = label;
+            this.mp = mp;
+        }
+
+        @Override
+        public void onAudioFocusChange(int focusChange) {
+            String event = "";
+
+            switch (focusChange) {
+                case AudioManager.AUDIOFOCUS_LOSS:
+                    event = "AUDIOFOCUS_LOSS";
+                    break;
+                case AudioManager.AUDIOFOCUS_LOSS_TRANSIENT:
+                    event = "AUDIOFOCUS_LOSS_TRANSIENT";
+                    break;
+                case AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK:
+                    event = "AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK";
+                    break;
+                case AudioManager.AUDIOFOCUS_GAIN:
+                    event = "AUDIOFOCUS_GAIN";
+                    break;
+            }
+            Log.i(TAG, label + " onAudioFocusChange: " + event);
+        }
     }
 }
