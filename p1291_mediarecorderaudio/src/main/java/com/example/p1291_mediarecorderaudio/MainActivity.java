6ef7e4cdf84b8244b977e5bcc36e2aab1e43d6aa
kmard <tsnok777@gmail.com>
2019-06-07 09:27:59 +0300

    p1301_audiorecorder

--- p1291_mediarecorderaudio/src/main/java/com/example/p1291_mediarecorderaudio/MainActivity.java
+++ p1291_mediarecorderaudio/src/main/java/com/example/p1291_mediarecorderaudio/MainActivity.java
@@ -1,13 +1,91 @@
 package com.example.p1291_mediarecorderaudio;
 
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MediaRecorder mediaRecorder;
+    private MediaPlayer mediaPlayer;
+    private String fileName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fileName = Environment.getExternalStorageDirectory() + "/record.3gpp";
+    }
+
+    public void recordStart(View v) {
+        try {
+            releaseRecorder();
+
+            File outFile = new File(fileName);
+            if (outFile.exists()) {
+                outFile.delete();
+            }
+
+            mediaRecorder = new MediaRecorder();
+            mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+            mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+            mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+            mediaRecorder.setOutputFile(fileName);
+            mediaRecorder.prepare();
+            mediaRecorder.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void releaseRecorder() {
+        if (mediaRecorder != null) {
+            mediaRecorder.release();
+            mediaRecorder = null;
+        }
+    }
+
+    public void recordStop(View view) {
+        if (mediaRecorder != null) {
+            mediaRecorder.stop();
+        }
+    }
+
+    public void playStart(View view) {
+        try {
+            releasePlayer();
+            mediaPlayer = new MediaPlayer();
+            mediaPlayer.setDataSource(fileName);
+            mediaPlayer.prepare();
+            mediaPlayer.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void releasePlayer() {
+        if (mediaPlayer != null) {
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+    }
+
+    public void playStop(View view) {
+        if (mediaPlayer != null) {
+            mediaPlayer.stop();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        releasePlayer();
+        releaseRecorder();
     }
 }
