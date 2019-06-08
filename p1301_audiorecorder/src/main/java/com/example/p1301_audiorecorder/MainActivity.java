e59d01a5582d174015cf30ce721ad34461d0861b
kmard <tsnok777@gmail.com>
2019-06-08 11:30:45 +0300

    p1311_cameraintent

--- p1301_audiorecorder/src/main/java/com/example/p1301_audiorecorder/MainActivity.java
+++ p1301_audiorecorder/src/main/java/com/example/p1301_audiorecorder/MainActivity.java
@@ -1,13 +1,94 @@
 package com.example.p1301_audiorecorder;
 
+import android.media.AudioFormat;
+import android.media.AudioRecord;
+import android.media.MediaRecorder;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "myLogs";
+
+    int myBufferSize = 8192;
+    AudioRecord audioRecord;
+    boolean isReading = false;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        createAudioRecorder();
+
+        Log.i(TAG, "init state = " + audioRecord.getState());
+    }
+
+    void createAudioRecorder() {
+        int sampleRate = 8000;
+        int channelConfig = AudioFormat.CHANNEL_IN_MONO;
+        int audioFormat = AudioFormat.ENCODING_PCM_16BIT;
+
+        int minInternalBufferSize = AudioRecord.getMinBufferSize(sampleRate,
+                channelConfig, audioFormat);
+        int internalBufferSize = minInternalBufferSize * 4;
+        Log.d(TAG, "minInternalBufferSize = " + minInternalBufferSize
+                + ", internalBufferSize = " + internalBufferSize
+                + ", myBufferSize = " + myBufferSize);
+
+        audioRecord = new AudioRecord(MediaRecorder.AudioSource.DEFAULT,
+                sampleRate, channelConfig, audioFormat, internalBufferSize);
+    }
+
+    public void recordStart(View view) {
+        Log.i(TAG, "recordStart: ");
+        audioRecord.startRecording();
+        int recordingState = audioRecord.getRecordingState();
+        Log.i(TAG, "recordingState " + recordingState);
+    }
+
+    public void recordStop(View view) {
+        Log.i(TAG, "recordStop: ");
+        audioRecord.stop();
+    }
+
+    public void readStart(View v) {
+        Log.i(TAG, "readStart: ");
+        isReading = true;
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                if (audioRecord == null)
+                    return;
+
+                byte[] myBuffer = new byte[myBufferSize];
+                int readCount = 0;
+                int totalCount = 0;
+                while (isReading) {
+                    readCount = audioRecord.read(myBuffer, 0, myBufferSize);
+                    totalCount += readCount;
+                    Log.i(TAG, "readCount = " + readCount + " , totalCount = " + totalCount);
+                }
+            }
+        }).start();
+    }
+
+    public void readStop(View v) {
+        Log.i(TAG, "readStop: ");
+        isReading = false;
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        isReading = false;
+        if (audioRecord != null) {
+            audioRecord.release();
+        }
     }
 }
