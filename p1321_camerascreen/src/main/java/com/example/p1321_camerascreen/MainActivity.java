9bf73ba41165864c31d22bde9a255c2095aab9e5
kmard <tsnok777@gmail.com>
2019-06-14 08:48:29 +0300

    p1321_camerascreen

--- p1321_camerascreen/src/main/java/com/example/p1321_camerascreen/MainActivity.java
+++ p1321_camerascreen/src/main/java/com/example/p1321_camerascreen/MainActivity.java
@@ -1,13 +1,95 @@
 package com.example.p1321_camerascreen;
 
+import android.hardware.Camera;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.Window;
+import android.view.WindowManager;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+
+    final int CAMERA_ID = 0;
+    final boolean FULL_SCREEN = true;
+    SurfaceView sv;
+    SurfaceHolder holder;
+    HolderCallback holderCallback;
+    Camera camera;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        sv = (SurfaceView) findViewById(R.id.surfaceView);
+        holder = sv.getHolder();
+        holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+
+        holderCallback = new HolderCallback();
+        holder.addCallback(holderCallback);
+
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        camera = Camera.open(CAMERA_ID);
+        setPreviewSize(FULL_SCREEN);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (camera != null)
+            camera.release();
+        camera = null;
+    }
+
+    private void setPreviewSize(boolean full_screen) {
+
+    }
+
+    void setCameraDisplayOrientation(int cameraId) {
+
+    }
+
+    private class HolderCallback implements SurfaceHolder.Callback {
+        @Override
+        public void surfaceCreated(SurfaceHolder holder) {
+            try {
+                camera.setPreviewDisplay(holder);
+                camera.startPreview();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+            camera.stopPreview();
+            setCameraDisplayOrientation(CAMERA_ID);
+            try {
+                camera.setPreviewDisplay(holder);
+                camera.startPreview();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }
+
+        @Override
+        public void surfaceDestroyed(SurfaceHolder holder) {
+
+        }
+    }
+
+
 }
