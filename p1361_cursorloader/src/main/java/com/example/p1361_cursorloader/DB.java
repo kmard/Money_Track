6c7214f130463e380075be1ea0387a5d3d300816
kmard <tsnok777@gmail.com>
2019-06-26 08:59:52 +0300

    p1361_cursorloader

--- p1361_cursorloader/src/main/java/com/example/p1361_cursorloader/DB.java
+++ p1361_cursorloader/src/main/java/com/example/p1361_cursorloader/DB.java
@@ -1,4 +1,57 @@
 package com.example.p1361_cursorloader;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
 public class DB {
+
+    public static final String COLUMN_ID = "_id";
+    public static final String COLUMN_IMG = "img";
+    public static final String COLUMN_TXT = "txt";
+    private static final String DB_NAME = "mydb";
+    private static final int DB_VERSION = 1;
+    private static final String DB_TABLE = "mytab";
+    private static final String DB_CREATE =
+            "create table " + DB_TABLE + "(" +
+                    COLUMN_ID + " integer primary key autoincrement, " +
+                    COLUMN_IMG + " integer, " +
+                    COLUMN_TXT + " text" +
+                    ");";
+
+
+    private final Context mCtx;
+
+    private DBHelper mDBHelper;
+    private SQLiteDatabase mDB;
+
+    public DB(Context ctx) {
+        mCtx = ctx;
+    }
+
+    //open connection
+    public void open() {
+        mDBHelper = new DBHelper(mCtx, DB_NAME, null, DB_VERSION);
+        mDB = mDBHelper.getWritableDatabase();
+    }
+
+    //close connection
+    public void close() {
+        if (mDBHelper != null) mDBHelper.close();
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
