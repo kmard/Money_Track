a0d29b830b29bd1f70d6f3341386f5d1f8875db1
kmard <tsnok777@gmail.com>
2019-07-05 09:56:14 +0300

    p1411_canvasview

---
+++ p1421_drawingfigure/src/main/java/com/example/p1421_drawingfigure/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.p1421_drawingfigure;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
