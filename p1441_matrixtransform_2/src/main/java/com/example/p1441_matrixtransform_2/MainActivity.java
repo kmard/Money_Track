637c57aee99a34bb02fe47d19ea4a31ece671c07
kmard <tsnok777@gmail.com>
2019-07-14 11:04:21 +0300

    p1441_matrixtransform_2

--- p1441_matrixtransform_2/src/main/java/com/example/p1441_matrixtransform_2/MainActivity.java
+++ p1441_matrixtransform_2/src/main/java/com/example/p1441_matrixtransform_2/MainActivity.java
@@ -30,37 +30,72 @@ public class MainActivity extends AppCompatActivity {
             matrix = new Matrix();
         }
 
+
+        //V1
+//        @Override
+//        protected void onDraw(Canvas canvas) {
+//            //super.onDraw(canvas);
+//
+//            canvas.drawARGB(80, 102, 204, 255);
+//
+//            //create cross in path
+//            path.reset();
+//            path.addRect(300, 150, 450, 200, Path.Direction.CW);
+//            path.addRect(350, 100, 400, 250, Path.Direction.CW);
+//
+//            //paint path as green
+//            p.setColor(Color.GREEN);
+//
+//            //custom matrix on change size
+//            //in 2 on horizontal
+//            //in 2,5 on vertical
+//            //relative point (375,100)
+//            matrix.reset();
+//            matrix.setScale(2f, 2.5f, 375, 100);
+//
+//            //add matrix to the path
+//            path.transform(matrix);
+//
+//            //paint path as blue
+//            p.setColor(Color.BLUE);
+//            canvas.drawPath(path, p);
+//
+//            //paint point relative which was been changed
+//            p.setColor(Color.BLACK);
+//            canvas.drawCircle(375, 100, 5, p);
+//        }
+
+        //V2
         @Override
         protected void onDraw(Canvas canvas) {
             //super.onDraw(canvas);
-
             canvas.drawARGB(80, 102, 204, 255);
 
             //create cross in path
             path.reset();
             path.addRect(300, 150, 450, 200, Path.Direction.CW);
             path.addRect(350, 100, 400, 250, Path.Direction.CW);
+            path.addCircle(375, 125, 5, Path.Direction.CW);
 
             //paint path as green
             p.setColor(Color.GREEN);
+            canvas.drawPath(path, p);
 
-            //custom matrix on change size
-            //in 2 on horizontal
-            //in 2,5 on vertical
-            //relative point (375,100)
+            //custom matrix rotate on 120 degree
+            //relative point (600,400)
             matrix.reset();
-            matrix.setScale(2f, 2.5f, 375, 100);
+            matrix.setRotate(120, 600, 400);
 
-            //add matrix to the path
+            //add matrix to path
             path.transform(matrix);
 
             //paint path as blue
             p.setColor(Color.BLUE);
             canvas.drawPath(path, p);
 
-            //paint point relative which was been changed
+            //paint point, relative which was done rotation
             p.setColor(Color.BLACK);
-            canvas.drawCircle(375, 100, 5, p);
+            canvas.drawCircle(600, 400, 5, p);
         }
     }
 }
