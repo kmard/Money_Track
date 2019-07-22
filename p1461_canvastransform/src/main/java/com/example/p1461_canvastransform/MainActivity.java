92f66a990bd5235b5b947baea0c4d3e2a9bfe7cc
kmard <tsnok777@gmail.com>
2019-07-22 09:36:17 +0300

    p1441_matrixtransform_2

--- p1461_canvastransform/src/main/java/com/example/p1461_canvastransform/MainActivity.java
+++ p1461_canvastransform/src/main/java/com/example/p1461_canvastransform/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.p1461_canvastransform;
 
 import android.content.Context;
-import android.graphics.*;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -15,55 +18,87 @@ public class MainActivity extends AppCompatActivity {
         setContentView(new DrawView(this));
     }
 
-    private class DrawView extends View {
 
-        Paint p;
-        Matrix matrix;
-        RectF rectf;
-        Path path;
 
-        public DrawView(Context context) {
-            super(context);
-            p = new Paint();
-            p.setStrokeWidth(3);
-            p.setStyle(Paint.Style.STROKE);
-            rectf = new RectF(100, 100, 200, 200);
-            matrix = new Matrix();
-            path = new Path();
-        }
-
-        @Override
-        protected void onDraw(Canvas canvas) {
-            canvas.drawARGB(80, 102, 204, 255);
-
-            //V1
-//            //square
-//            path.reset();
-//            path.addRect(rectf,Path.Direction.CW);
-//            p.setColor(Color.BLACK);
-//            canvas.drawPath(path,p);
+//    private class DrawView extends View {
 //
-//            //transformed square
-//            matrix.reset();
-//            matrix.preRotate(30);
-//            matrix.preTranslate(500,0);
-//            path.transform(matrix);
-//            p.setColor(Color.BLUE);
-//            canvas.drawPath(path,p);
+//        Paint p;
+//        Matrix matrix;
+//        RectF rectf;
+//        Path path;
+//
+//        public DrawView(Context context) {
+//            super(context);
+//            p = new Paint();
+//            p.setStrokeWidth(3);
+//            p.setStyle(Paint.Style.STROKE);
+//            rectf = new RectF(100, 100, 200, 200);
+//            matrix = new Matrix();
+//            path = new Path();
+//        }
+//
+//        @Override
+//        protected void onDraw(Canvas canvas) {
+//            canvas.drawARGB(80, 102, 204, 255);
+//
+//            //V1
+////            //square
+////            path.reset();
+////            path.addRect(rectf,Path.Direction.CW);
+////            p.setColor(Color.BLACK);
+////            canvas.drawPath(path,p);
+////
+////            //transformed square
+////            matrix.reset();
+////            matrix.preRotate(30);
+////            matrix.preTranslate(500,0);
+////            path.transform(matrix);
+////            p.setColor(Color.BLUE);
+////            canvas.drawPath(path,p);
+//
+//
+//            //V2
+//
+//            //square
+//            p.setColor(Color.BLACK);
+//            canvas.drawRect(rectf, p);
+//
+//            //square in canvas with transform
+//            canvas.rotate(30);
+//            canvas.translate(500, 000);
+//            p.setColor(Color.GREEN);
+//            canvas.drawRect(rectf, p);
+//
+//        }
+//    }
 
+//    V3
+private class DrawView extends View {
 
-            //V2
+    Paint p;
+    RectF rectf1;
+    RectF rectf2;
 
-            //square
-            p.setColor(Color.BLACK);
-            canvas.drawRect(rectf, p);
-
-            //square in canvas with transform
-            canvas.rotate(30);
-            canvas.translate(500, 000);
-            p.setColor(Color.GREEN);
-            canvas.drawRect(rectf, p);
-
-        }
+    public DrawView(Context context) {
+        super(context);
+        p = new Paint();
+        p.setStrokeWidth(3);
+        p.setStyle(Paint.Style.STROKE);
+        rectf1 = new RectF(50, 50, 100, 100);
+        rectf2 = new RectF(50, 150, 100, 200);
     }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+      canvas.drawARGB(80,102,204,255);
+
+      //green square
+        p.setColor(Color.GREEN);
+        canvas.drawRect(rectf1,p);
+
+        //blue square
+        p.setColor(Color.BLUE);
+        canvas.drawRect(rectf2,p);
+    }
+}
 }
