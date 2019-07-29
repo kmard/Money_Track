2e2197e5f4b36e5e7a8c8e4e0600ebe36ea73e44
kmard <tsnok777@gmail.com>
2019-07-29 10:31:08 +0300

    p1501_pathmeasure

--- p1501_pathmeasure/src/main/java/com/example/p1501_pathmeasure/MainActivity.java
+++ p1501_pathmeasure/src/main/java/com/example/p1501_pathmeasure/MainActivity.java
@@ -1,13 +1,64 @@
 package com.example.p1501_pathmeasure;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.PathMeasure;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "myLogs";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(new DrawView(this));
+    }
+
+    private class DrawView extends View {
+
+        Paint paint;
+        Paint paintText;
+        Path path;
+        PathMeasure pMeasure;
+        float length;
+
+        public DrawView(Context context) {
+            super(context);
+            paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+            paint.setStyle(Paint.Style.STROKE);
+            paint.setStrokeWidth(3);
+
+            paintText = new Paint(Paint.ANTI_ALIAS_FLAG);
+            paintText.setTextSize(30);
+
+            path = new Path();
+            path.moveTo(100, 300);
+            path.rLineTo(150, 100);
+            path.rLineTo(150, -100);
+            path.rQuadTo(150, 200, 300, 0);
+            path.rLineTo(150, 100);
+            path.rLineTo(150, -100);
+
+            pMeasure = new PathMeasure(path, false);
+            length = pMeasure.getLength();
+        }
+
+        @Override
+        protected void onDraw(Canvas canvas) {
+            canvas.drawARGB(80, 102, 204, 255);
+
+            canvas.drawPath(path, paint);
+            canvas.drawText(String.format("Length : %s ", length), 100, 100, paintText);
+        }
     }
 }
