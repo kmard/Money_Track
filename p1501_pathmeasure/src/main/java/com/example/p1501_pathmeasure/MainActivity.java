afb4edfc23f98ebbee0bcafc75cd530502977569
kmard <tsnok777@gmail.com>
2019-07-30 08:36:09 +0300

    p1501_pathmeasure

--- p1501_pathmeasure/src/main/java/com/example/p1501_pathmeasure/MainActivity.java
+++ p1501_pathmeasure/src/main/java/com/example/p1501_pathmeasure/MainActivity.java
@@ -1,15 +1,15 @@
 package com.example.p1501_pathmeasure;
 
 import android.content.Context;
-import android.graphics.Canvas;
-import android.graphics.Paint;
-import android.graphics.Path;
-import android.graphics.PathMeasure;
+import android.graphics.*;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+
+import java.util.Arrays;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,16 +24,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(new DrawView(this));
     }
 
+    //V2
     private class DrawView extends View {
 
         Paint paint;
         Paint paintText;
         Path path;
         PathMeasure pMeasure;
+        Matrix matrix;
+        Rect rect;
+
+        float[] pos;
+        float[] tan;
+
         float length;
+        float distance;
 
         public DrawView(Context context) {
             super(context);
+
             paint = new Paint(Paint.ANTI_ALIAS_FLAG);
             paint.setStyle(Paint.Style.STROKE);
             paint.setStrokeWidth(3);
@@ -42,23 +51,84 @@ public class MainActivity extends AppCompatActivity {
             paintText.setTextSize(30);
 
             path = new Path();
-            path.moveTo(100, 300);
-            path.rLineTo(150, 100);
-            path.rLineTo(150, -100);
-            path.rQuadTo(150, 200, 300, 0);
-            path.rLineTo(150, 100);
-            path.rLineTo(150, -100);
+            path.moveTo(100,300);
+            path.rLineTo(150,100);
+            path.rLineTo(150,-100);
+            path.rQuadTo(150,200,300,0);
+            path.rLineTo(150,100);
+            path.rLineTo(150,-100);
 
-            pMeasure = new PathMeasure(path, false);
+            pMeasure = new PathMeasure(path,false);
+
             length = pMeasure.getLength();
+            distance = length/4;
+
+            matrix = new Matrix();
+            pMeasure.getMatrix(distance,matrix,
+                    PathMeasure.POSITION_MATRIX_FLAG
+            +PathMeasure.TANGENT_MATRIX_FLAG);
+
+            pos = new float[2];
+            tan = new float[2];
+            pMeasure.getPosTan(distance,pos,tan);
+
+            rect = new Rect(-20,-10,20,10);
         }
 
         @Override
         protected void onDraw(Canvas canvas) {
-            canvas.drawARGB(80, 102, 204, 255);
+            canvas.drawARGB(80,102,204,255);
 
-            canvas.drawPath(path, paint);
-            canvas.drawText(String.format("Length : %s ", length), 100, 100, paintText);
+            canvas.drawPath(path,paint);
+            canvas.drawText(
+                    String.format("Distance : %s of %s ",distance,length),100,
+                    100, paintText);
+            canvas.drawText(
+                    String.format("Position : %s. Tangent (cos,sin): %s",
+                            Arrays.toString(pos),Arrays.toString(tan)),100,
+                    150,paintText);
+            canvas.setMatrix(matrix);
+            canvas.drawRect(rect,paint);
         }
     }
+
+
+    //V1
+//    private class DrawView extends View {
+//
+//        Paint paint;
+//        Paint paintText;
+//        Path path;
+//        PathMeasure pMeasure;
+//        float length;
+//
+//        public DrawView(Context context) {
+//            super(context);
+//            paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+//            paint.setStyle(Paint.Style.STROKE);
+//            paint.setStrokeWidth(3);
+//
+//            paintText = new Paint(Paint.ANTI_ALIAS_FLAG);
+//            paintText.setTextSize(30);
+//
+//            path = new Path();
+//            path.moveTo(100, 300);
+//            path.rLineTo(150, 100);
+//            path.rLineTo(150, -100);
+//            path.rQuadTo(150, 200, 300, 0);
+//            path.rLineTo(150, 100);
+//            path.rLineTo(150, -100);
+//
+//            pMeasure = new PathMeasure(path, false);
+//            length = pMeasure.getLength();
+//        }
+//
+//        @Override
+//        protected void onDraw(Canvas canvas) {
+//            canvas.drawARGB(80, 102, 204, 255);
+//
+//            canvas.drawPath(path, paint);
+//            canvas.drawText(String.format("Length : %s ", length), 100, 100, paintText);
+//        }
+//    }
 }
