032d31721538b17f4885c062e49bfb931c5ca059
kmard <tsnok777@gmail.com>
2019-08-01 09:19:26 +0300

    p1511_patheffect

--- p1511_patheffect/src/main/java/com/example/p1511_patheffect/MainActivity.java
+++ p1511_patheffect/src/main/java/com/example/p1511_patheffect/MainActivity.java
@@ -16,12 +16,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(new DrawView(this));
     }
 
+    //DiscretePathEffect
     private class DrawView extends View {
 
         Path path;
         Paint p1;
         Paint p2;
         Paint p3;
+
 
         public DrawView(Context context) {
             super(context);
@@ -37,12 +39,11 @@ public class MainActivity extends AppCompatActivity {
 
             p2 = new Paint(p1);
             p2.setColor(Color.GREEN);
-            p2.setPathEffect(new CornerPathEffect(25));
+            p2.setPathEffect(new DiscretePathEffect(10, 5));
 
             p3 = new Paint(p1);
             p3.setColor(Color.BLUE);
-            p3.setPathEffect(new CornerPathEffect(50));
-
+            p3.setPathEffect(new DiscretePathEffect(10, 15));
         }
 
         @Override
@@ -57,7 +58,53 @@ public class MainActivity extends AppCompatActivity {
 
             canvas.translate(250, 0);
             canvas.drawPath(path, p3);
-
         }
     }
+
+
+    //CornerPathEffect
+//    private class DrawView extends View {
+//
+//        Path path;
+//        Paint p1;
+//        Paint p2;
+//        Paint p3;
+//
+//        public DrawView(Context context) {
+//            super(context);
+//
+//            path = new Path();
+//            path.rLineTo(100, 300);
+//            path.rLineTo(100, -100);
+//            path.rLineTo(100, 300);
+//
+//            p1 = new Paint(Paint.ANTI_ALIAS_FLAG);
+//            p1.setStyle(Paint.Style.STROKE);
+//            p1.setStrokeWidth(3);
+//
+//            p2 = new Paint(p1);
+//            p2.setColor(Color.GREEN);
+//            p2.setPathEffect(new CornerPathEffect(25));
+//
+//            p3 = new Paint(p1);
+//            p3.setColor(Color.BLUE);
+//            p3.setPathEffect(new CornerPathEffect(50));
+//
+//        }
+//
+//        @Override
+//        protected void onDraw(Canvas canvas) {
+//            canvas.drawARGB(80, 102, 204, 255);
+//
+//            canvas.translate(100, 100);
+//            canvas.drawPath(path, p1);
+//
+//            canvas.translate(250, 0);
+//            canvas.drawPath(path, p2);
+//
+//            canvas.translate(250, 0);
+//            canvas.drawPath(path, p3);
+//
+//        }
+//    }
 }
