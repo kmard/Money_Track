09d6f92a29b06b905d5167bde964e820bf73c82d
kmard <tsnok777@gmail.com>
2019-08-09 10:03:23 +0300

    p1571_bitmapread

--- p1561_avoidxfermode/src/main/java/com/example/p1561_avoidxfermode/MainActivity.java
+++ p1561_avoidxfermode/src/main/java/com/example/p1561_avoidxfermode/MainActivity.java
@@ -1,13 +1,60 @@
 package com.example.p1561_avoidxfermode;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.graphics.*;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(new DrawView(this));
+    }
+
+    private class DrawView extends View {
+
+        Paint paint;
+
+        Rect rect;
+
+        Bitmap bitmap;
+        PorterDuff.Mode mode = PorterDuff.Mode.DARKEN;
+
+
+        public DrawView(Context context) {
+            super(context);
+
+            paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+            rect = new Rect(0,0,50,100);
+            createBitmap();
+        }
+
+
+        @Override
+        protected void onDraw(Canvas canvas) {
+            canvas.drawARGB(80,102,204,255);
+            canvas.drawBitmap(bitmap,0,0,paint);
+        }
+
+        private void createBitmap() {
+            //create bitmap
+            bitmap = Bitmap.createBitmap(1100,700,Bitmap.Config.ARGB_8888);
+            Canvas bitmapCanvas = new Canvas(bitmap);
+
+            //create brush for use
+            Paint redPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+            redPaint.setStyle(Paint.Style.FILL_AND_STROKE);
+            redPaint.setColor(Color.RED);
+
+            //drawing rectangular
+          //  drawBitmap(bitmapCanvas redPaint);
+
+            //tuning for brush
+            //redPaint.setXfermode(new AvoidXfermode(Color.BLUE,0,mode));
+
+        }
     }
 }
---
+++ p1571_bitmapread/src/main/java/com/example/p1571_bitmapread/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.p1571_bitmapread;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
